82572fdc6f0b29e2b0f925ace6c1cedd8749b797
Sovereign-Absolute <runner@runnervmymu0l.4xgwln4iv0kutoom5mu5zvxmxb.bx.internal.cloudapp.net>
2026-01-25 20:41:47 +0000

    Sovereign: Absolute Correction Applied

--- buildozer.spec
+++ buildozer.spec
@@ -1,10 +1,9 @@
 [app]
-title = Kajitan Sovereign
-package.name = kajitan.ai
-source.dir = .
-version = 2.5.0
-requirements = python3,kivy,opencv,numpy,android
-android.archs = arm64-v8a
-android.api = 33
-android.ndk = 25b
-android.permissions = INTERNET,CAMERA
+title=Kajitan
+package.name=kj.ai
+source.dir=.
+version=1.0
+requirements=python3,kivy,opencv,numpy,android
+android.archs=arm64-v8a
+android.api=33
+android.ndk=25b
--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.core.window import Window
-from kivy.clock import Clock
-class KajitanUI(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = 'vertical'
-        Window.clearcolor = (0, 0, 0, 1)
-        self.add_widget(Label(text='[b][color=FFD700]KAJITAN SOVEREIGN[/color][/b]', markup=True, font_size='40sp'))
-        self.status = Label(text='Scanning Field...', font_size='18sp')
-        self.add_widget(self.status)
-        Clock.schedule_interval(self.update, 0.1)
-    def update(self, dt):
-        self.status.text = 'Calculating 5 Trillion Cycles... [color=FF0000]AIM LOCKED[/color]'
-        self.status.markup = True
-class KajitanApp(App):
+class Kajitan(App):
     def build(self):
-        return KajitanUI()
-if __name__ == '__main__': KajitanApp().run()
+        Window.clearcolor = (0,0,0,1)
+        return Label(text='[color=FFD700][b]KAJITAN SOVEREIGN[/b][/color]', markup=True, font_size='40sp')
+Kajitan().run()
